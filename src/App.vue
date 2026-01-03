03b9a07fa8460dcd847895fa72808d005bfa0085
devbattery <devbattery@outlook.com>
2026-01-03 19:33:18 +0900

    Feat: JobListing Component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 
 <template>
@@ -9,4 +10,5 @@ import HomeCards from "@/components/HomeCards.vue";
   <Hero title="Test Title" subtitle="Test SubTitle" />
   <!--  <Hero />-->
   <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
